3bd9223b08a0a4ff7b0c172ba7b687b449e5685e
austin fujimori <austinfujimori@childrens-imac.hsd1.ca.comcast.net>
2019-01-27 10:48:13 -0800

    Initial Commit

---
+++ testprojectTests/testprojectTests.swift
@@ -0,0 +1,34 @@
+//
+//  testprojectTests.swift
+//  testprojectTests
+//
+//  Created by austin fujimori on 1/27/19.
+//  Copyright © 2019 austin fujimori. All rights reserved.
+//
+
+import XCTest
+@testable import testproject
+
+class testprojectTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
